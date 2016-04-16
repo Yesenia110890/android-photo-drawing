6a3db2634e4baff6408c7d8d1649587e866599db
Yesenia Isabel <edye_1509@live.com.mx>
2016-04-16 16:51:42 -0500

    feat(photos): Add controller to take photo

--- app/src/main/java/com/flor/photodrawing/PhotoActivity.java
+++ app/src/main/java/com/flor/photodrawing/PhotoActivity.java
@@ -1,6 +1,9 @@
 package com.flor.photodrawing;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,45 +11,63 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 
 public class PhotoActivity extends AppCompatActivity {
+
+    @Bind(R.id.save)
+    FloatingActionButton fabSave;
+
+    @Bind(R.id.imv_photo)
+    ImageViewTouch imvPhoto;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        initProperties();
+    }
+
+    public void initProperties() {
+
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
+        fabSave.setEnabled(false);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.photo);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+    }
+
+    @OnClick(R.id.save)
+    public void savePhoto(View v) {
+        Snackbar.make(v, "Save", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+    @OnClick(R.id.photo)
+    public void drawingPhoto(View v) {
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, 0);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_photo, menu);
-        return true;
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            Bitmap mBitmap = (Bitmap) data.getExtras().get("data");
+            imvPhoto.setImageBitmap(mBitmap);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        } catch (NullPointerException e) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
